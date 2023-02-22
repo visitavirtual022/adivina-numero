9c31be01fa372eecda94ce0ae5e99e2f217874ea
juanda <juandacorreo@gmail.com>
2023-02-22 14:03:06 +0100

    Implementada jugar partida

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const guessField = document.querySelector('.guess')
 const highscoreField = document.querySelector('.highscore')
 const messageField = document.querySelector('.message')
 const numberField = document.querySelector('.number')
+const bodyField = document.querySelector('body')
 
 // 2do paso: crear las variables que necesitamos
 
 let score = 20
-const highscore = 0
+let highscore = 0
 
 // obtener un número aleatorio entre 1 y 20
 const MIN_NUMBER = 1
@@ -23,18 +24,31 @@ console.log(`El número secreto es ${secretNumber}`)
 // un mensaje que diga si el número es mayor o menor  en el campo messageField
 // ...
 
-checkButton.addEventListener('click', function () {
-  const number = guessField.value
-  console.log(`El número ingresado es ${number}`)
-  if (number < secretNumber) {
-    messageField.textContent = 'El número es menor'
-    score--
-    scoreField.textContent = score
-  } else if (number > secretNumber) {
-    messageField.textContent = 'El número es mayor'
-    score--
-    scoreField.textContent = score
+checkButton.addEventListener('click', fnCheckButton)
+
+function mostrarMensaje(mensaje) {
+  messageField.textContent = mensaje
+}
+
+function fnCheckButton() {
+  // aunque guesssField  sea un input de tipo number, su valor es un string
+  const number = Number(guessField.value)
+
+  if (number === secretNumber) {
+    mostrarMensaje('¡Ganaste!')
+    if (score > highscore) highscore = highscoreField.textContent = score
+    // cambiar el color del fondo del body o lo que sea, mostrar el número secreto en vez de ?
+    bodyField.style.backgroundColor = '#60b347'
+    numberField.textContent = secretNumber
+  } else if (score === 1) {
+    mostrarMensaje('¡Perdiste!')
+    scoreField.textContent = 0
+    bodyField.style.backgroundColor = '#b34747'
   } else {
-    messageField.textContent = '¡Ganaste!'
+    const mensaje =
+      number > secretNumber ? 'El número es mayor' : 'El número es menor'
+    mostrarMensaje(mensaje)
+    score--
+    scoreField.textContent = score
   }
-})
+}
