ce077f8c3467a7ee620f88f2d1010eb1e27944df
javiercasado022 <javiercasado022@gmail.com>
2024-01-15 12:03:22 +0100

    subir script js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+//configurar la aplicacion
+const mensajeInicial = 'Empieza a adivinar'
+let score = 21
+let highScore = 1
+const secretNumber = Math.floor(Math.random()*20+1)
+// seleccionar elementos del DOM
+const messageField = document.querySelector('.message')
+const scoreField = document.querySelector('.score')
+const highScoreField = document.querySelector('.highscore')
+const secretNumberField = document.querySelector('.number')
+const checkButton = document.querySelector('.check')
+const againButton = document.querySelector('.again')
+const guessNumberField = document.querySelector('.guess').value
+
+//inicializar la aplicación
+messageField.textContent= mensajeInicial
+scoreField.textContent = score
+highScoreField.textContent = highScore
+secretNumberField.textContent = secretNumber
+
+//funcionalidad de la aplicación
+
+//addEventListener es una funcion que recibe como argumento otra función
+checkButton.addEventListener('click', () => {
+    // obtener el valor del input
+const guessNumber = Number (guessNumberField.value)
+
 
